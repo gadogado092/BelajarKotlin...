faa468929fedd1e67f96ed723c891900363bacb3
RABIULDIEN AMAT <34974676+gadogado092@users.noreply.github.com>
2018-08-19 18:31:02 +0800

    Initial commit

---
+++ app/src/main/java/amat/belajarkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package amat.belajarkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun ubah(view:View){
+        imageView2.setImageResource(R.drawable.slank2)
+    }
+
+}
